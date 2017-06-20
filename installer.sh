0804d3a80ad74507f1b8b1cc9f08dc24381deb45
Gael Porte <porte.gael@hotmail.fr>
2017-06-21 00:00:12 +0200

    Add package to installer script

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh   
 
-list=( "git" "vim" "zsh" )   
+list=( "git" "vim" "zsh" "python-pip" "python-smbus" "python-serial" "cmake" "libusb-1.0" "minicom")   
 for element in "${list[@]}"    
     do
 	sudo apt-get install -y $element
